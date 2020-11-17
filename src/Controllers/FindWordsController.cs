32c1c6a5fa552e64f84fba21afda3a44722f4bf8
David Jarman <davidbjarman@gmail.com>
2020-11-17 15:54:07 -0800

    Add base project

---
+++ src/Controllers/FindWordsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SpellingBee.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FindWordsController : ControllerBase
+    {
+        private readonly ILogger<FindWordsController> _logger;
+
+        public FindWordsController(ILogger<FindWordsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "ayy";
+        }
+    }
+}
