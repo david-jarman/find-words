8e7195f7b0de1a775e1706088b930634669d09df
David Jarman <davidbjarman@gmail.com>
2020-12-13 15:07:54 -0800

    Add Lets Encrypt support

--- src/Startup.cs
+++ src/Startup.cs
@@ -12,12 +12,14 @@ namespace SpellingBee
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,12 +35,17 @@ namespace SpellingBee
                 string[] words = File.ReadAllLines("words.txt");
                 return words.Where(words => words.Length > 3).ToArray();
             });
+
+            if (!Env.IsDevelopment())
+            {
+                services.AddLettuceEncrypt();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (Env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
